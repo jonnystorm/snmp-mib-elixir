416ce0da0ee077a2b510d7f26c6516293025229a
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-12-30 17:09:47 -0600

    Added timeticks type

--- lib/snmp_mib.ex
+++ lib/snmp_mib.ex
@@ -93,7 +93,7 @@ defmodule SNMPMIB do
       boolean: 1,
       integer: 2, counter: 2, counter32: 2, counter64: 2,
       bit_string: 3,
-      octet_string: 4, string: 4,
+      octet_string: 4, string: 4, timeticks: 4,
       ipaddress: 4,  # IpAddress type obsoleted by RFC2851
       null: 5,
       object_identifier: 6, oid: 6,
